bd4b5e17a634803c416855a23c03a0228de6c00b
Brian Landrón <blandron@optivon.net>
2017-01-23 13:41:57 -0400

    officially migrated our mercurio-sdk to be included in our web-interface repo

--- public/lib/mercurio-sdk/MissingImplementationError.js
+++
@@ -1,9 +0,0 @@
-/*
-@constructor: Constructor for MissingImplementationError
-@params: message - message to describe error
-*/
-
-function MissingImplementationError(message){
-	this.message = message;
-	this.name = "Missing Implementation Error";
-}
